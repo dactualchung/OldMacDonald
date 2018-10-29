b58c58bc71e461f6e29af797f777d40953394012
dactualchung <dachung1@s.sfusd.edu>
2018-10-29 11:45:36 -0700

    Added initial OldMacDonald code

--- Chick.java
+++ Chick.java
@@ -1,4 +1,24 @@
-class Chick //implements Animal 
-{     
-     //your code here
+class Chick implements Animal  
+{
+  private String myType, mySoundOne, mySoundTwo;
+  public Chick(String type, String soundOne, String soundTwo){
+    myType = type;
+    mySoundOne = soundOne;
+    mySoundTwo = soundTwo;
+  }
+  public Chick() {
+    myType = "unknown";
+    mySoundOne = "unknown";
+    mySoundTwo = "unknown";
+  }
+  public String getSound(){
+    if(Math.random() < 0.5){
+      return mySoundOne;
+    }else{
+      return mySoundTwo;
+    }
+  }
+  public String getType(){
+    return myType;
+  }
 }
